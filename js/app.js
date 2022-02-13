a62b0d1883ec43a9a8303cb583ce781246ffc110
cloud3999 <edwardlcloud@gmail.com>
2022-02-13 16:50:57 -0500

    Successful in adding X's and O's to squares
    Still to do: determine and declare winner, set status to not update when clicking an already filled cell, make cells unplayable after game finish

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 const gameBoard = document.querySelector(".gameBoard")
 //const playerTurnStatus = document.querySelector("#status")
 const resetButton = document.querySelector(".reset")
+const sq1 = document.querySelector("#sq1")
+const sq2 = document.querySelector("#sq2")
+const sq3 = document.querySelector("#sq3")
+const sq4 = document.querySelector("#sq4")
+const sq5 = document.querySelector("#sq5")
+const sq6 = document.querySelector("#sq6")
+const sq7 = document.querySelector("#sq7")
+const sq8 = document.querySelector("#sq8")
+const sq9 = document.querySelector("#sq9")
+
+
+
+
+
+
 
 
 // const playerTurnX = document.createElement("div");
@@ -20,7 +35,8 @@ const resetButton = document.querySelector(".reset")
 
 //(number % 2 === 0) maybe use if else to toggle clicks. May need a click event counter
 let playerTurn = 0
-
+let currentPlayer = ""
+let gameState = ["", "", "", "", "", "", "", "", ""];
 
 // // if (playerTurn % 2 === 0) {
 //     "O"
@@ -49,55 +65,123 @@ const winningConditions = [
   ];
 
 
-  const resetBoard = () => {
-      console.log("resetButton was clicked")
 
-  }
+
+//   const resetBoard = () => {
+//      console.log("resetButton was clicked")
+
+//   }
+
+
+
 
 const clickCell = () => {
 
     const playerTurnStatus = document.querySelector("#status")
     
     playerTurn += 1
-    // let element = document.getElementsByClassName('boardSquare');
-    // for(let i=0;i<element.length;i++){
-    //   if(element[i].innerHTML == ""){
+   
     //     element[i].textContent = "X"
-    //   } else{
-    //     console.log("ran")
-    //   }
-    // }
-
+    
     if (playerTurn % 2 === 0) {
         const playerTurnO = document.createElement("div");
         playerTurnO.id = "status";
-        playerTurnO.innerHTML = "<p> It is O's Turn </p>"
+        playerTurnO.innerHTML = "<p> It is X's Turn </p>"
         playerTurnStatus.replaceWith(playerTurnO)
+        currentPlayer= "X"
     
     } else {
         const playerTurnX = document.createElement("div");
         playerTurnX.id = "status";
-        playerTurnX.innerHTML = "<p> It is X's Turn</p>"
+        playerTurnX.innerHTML = "<p> It is O's Turn</p>"
         playerTurnStatus.replaceWith(playerTurnX)
+        currentPlayer = "O"
 
     }
 
+
+// const handlePlayerChange = () => {
+//     if (playerTurn % 2 === 0) {
+//         currentPlayer = "X";
+//     }else {
+//     currentPlayer = "O";
+//     }
+// }
+
     
-
+    console.log(currentPlayer)
     console.log("the number of times clicked is" , playerTurn)
-    // if (playerTurn % 2 == 0) {
-    //     console.log("playerTurn Even")
-    // } else {
-    //     console.log("player turn odd")
-    // }
-   
+}
 
+const turnPlay1 = () => {
+    //if cell is "" then currentPlayer
+    if (sq1.innerText === "") {
+    sq1.textContent = currentPlayer
+    }
+}
+const turnPlay2 = () => {
+    //if cell is "" then currentPlayer
+    if (sq2.innerText === "") {
+    sq2.textContent = currentPlayer
+    }
+}
+const turnPlay3 = () => {
+    //if cell is "" then currentPlayer
+    if (sq3.innerText === "") {
+    sq3.textContent = currentPlayer
+    }
+}
+const turnPlay4 = () => {
+    //if cell is "" then currentPlayer
+    if (sq4.innerText === "") {
+    sq4.textContent = currentPlayer
+    }
+}
+const turnPlay5 = () => {
+    //if cell is "" then currentPlayer
+    if (sq5.innerText === "") {
+    sq5.textContent = currentPlayer
+    }
+}
+const turnPlay6 = () => {
+    //if cell is "" then currentPlayer
+    if (sq6.innerText === "") {
+    sq6.textContent = currentPlayer
+    }
+}
+const turnPlay7 = () => {
+    //if cell is "" then currentPlayer
+    if (sq7.innerText === "") {
+    sq7.textContent = currentPlayer
+    }
+}
+const turnPlay8 = () => {
+    //if cell is "" then currentPlayer
+    if (sq8.innerText === "") {
+    sq8.textContent = currentPlayer
+    }
+}
+const turnPlay9 = () => {
+    //if cell is "" then currentPlayer
+    if (sq9.innerText === "") {
+    sq9.textContent = currentPlayer
+    }
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
     gameBoard.addEventListener("click", clickCell)
-    resetButton.addEventListener("click", resetBoard)
+    sq1.addEventListener("click", turnPlay1)
+    sq2.addEventListener("click", turnPlay2)
+    sq3.addEventListener("click", turnPlay3)
+    sq4.addEventListener("click", turnPlay4)
+    sq5.addEventListener("click", turnPlay5)
+    sq6.addEventListener("click", turnPlay6)
+    sq7.addEventListener("click", turnPlay7)
+    sq8.addEventListener("click", turnPlay8)
+    sq9.addEventListener("click", turnPlay9)
+
+    //resetButton.addEventListener("click", resetBoard)
     
 
 
